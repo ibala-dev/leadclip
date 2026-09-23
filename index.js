d82e586c26840f0259dfce907fb1a8f3d619f80f
ibala-dev <balaganesh.dharmaraj07@gmail.com>
2026-09-23 14:51:28 +0530

    delete all button functionality added

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
 let myLeads = []
-let inputEl = document.getElementById("input-el")
-let inputButton = document.getElementById("input-btn")
-let ulEl = document.getElementById("ul-l")
-let mylead = `["www.google.com]`
+const inputEl = document.getElementById("input-el")
+const inputButton = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-l")
+const leadsFromLocalStorage = localStorage.getItem("myLeads")
+let actualItem = leadsFromLocalStorage ? JSON.parse(leadsFromLocalStorage) : null
+let deleteButton = document.getElementById("delete-btn")
+
+if (actualItem) {
+    myLeads = actualItem
+    renderLeads()
+}
+
+else console.log("falsy value")
 
 inputButton.addEventListener("click", () => {
-    myLeads.push(inputEl.value)
-    inputEl.value = ''  
-    localStorage.setItem("myLeads",JSON.stringify(myLeads))
-    renderLeads()
-    console.log("myLeads")
+    let inputValue = inputEl.value.trim()
+    if (inputValue) {
+        myLeads.push(inputValue)
+        inputEl.value = ""
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        renderLeads()
+
+    }
 })
+
 function renderLeads() {
-    
-    lead = myLeads.length - 1
-    let a = document.createElement("a")
-    lead = myLeads[lead]
-    a.href = "lead"
-    a.target="_blank"
-    a.textContent = lead
-    let li = document.createElement("li")
-    li.appendChild(a)
-    ulEl.appendChild(li)
+    ulEl.innerHTML=''
+    myLeads.forEach(lead => {
+        let a = document.createElement("a")
+        a.href = "#"
+        a.target = "_blank"
+        a.textContent = lead
+        let li = document.createElement("li")
+        li.appendChild(a)
+        ulEl.appendChild(li)
+
+    });
 }
+
+deleteButton.addEventListener("click",()=>{
+        localStorage.removeItem("myLeads")
+        myLeads.length=0
+        renderLeads()
+})
